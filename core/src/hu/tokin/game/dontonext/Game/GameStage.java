d03230afeb842572983625f8552c5fd34b3c9b71
Mátyás Dávid <davimatyi@outlook.com>
2018-02-02 09:57:20 +0100

    NEM LOPOT VILAG ELADDO

--- core/src/hu/tokin/game/dontonext/Game/GameStage.java
+++ core/src/hu/tokin/game/dontonext/Game/GameStage.java
@@ -6,6 +6,13 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -16,6 +23,7 @@ import java.util.ArrayList;
 
 import hu.tokin.game.dontonext.Globals.Assets;
 import hu.tokin.game.dontonext.Globals.Globals;
+import hu.tokin.game.dontonext.MyBaseClasses.Box2dWorld.WorldBodyEditorLoader;
 import hu.tokin.game.dontonext.MyBaseClasses.Scene2D.MyActor;
 import hu.tokin.game.dontonext.MyBaseClasses.Scene2D.MyStage;
 import hu.tokin.game.dontonext.MyBaseClasses.Scene2D.OneSpriteStaticActor;
@@ -29,17 +37,24 @@ import static hu.tokin.game.dontonext.Globals.Globals.pintsz;
 
 public class GameStage extends MyStage {
 
-    private ControlStage controlStage;
+    //private ControlStage controlStage;
+
+    World world;
+    Box2DDebugRenderer box2DDebugRenderer;
+    WorldBodyEditorLoader loader;
+
+    int rdm(int a, int b){return (int)(Math.random()*(b-a+1)+a);}
+    float randomF(float a, float b){return (float) (Math.random()*(b-a+1)+a);}
 
 
     public GameStage(Viewport viewport, Batch batch, MyGdxGame game) {
         super(viewport, batch, game);
-        controlStage = new ControlStage(new ExtendViewport(Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT, new OrthographicCamera(Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT)),new SpriteBatch(), game, this);
+        //controlStage = new ControlStage(new ExtendViewport(Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT, new OrthographicCamera(Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT)),new SpriteBatch(), game, this);
 
 
         InputMultiplexer inputMultiplexer = new InputMultiplexer();
         inputMultiplexer.addProcessor(this);
-        inputMultiplexer.addProcessor(controlStage);
+        //inputMultiplexer.addProcessor(controlStage);
         Gdx.input.setInputProcessor(inputMultiplexer);
 
     }
@@ -47,9 +62,44 @@ public class GameStage extends MyStage {
 
     @Override
     public void init() {
-        //addActor(new Wall(0,0));
+        world = new World(new Vector2(0, -2.5f), false);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+        loader = new WorldBodyEditorLoader(Gdx.files.internal("fizika.json"));
+
+
+
+        world.setContactListener(new ContactListener() {
+            @Override
+            public void beginContact(Contact contact) {
+
+            }
+
+            @Override
+            public void endContact(Contact contact) {
+
+            }
+
+            @Override
+            public void preSolve(Contact contact, Manifold oldManifold) {
+
+            }
+
+            @Override
+            public void postSolve(Contact contact, ContactImpulse impulse) {
+
+            }
+        });
 
     }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.BACK){
+            game.setScreenBackByStackPop();
+        }
+        return false;
+    }
+
 
     @Override
     public void dispose() {
@@ -59,7 +109,7 @@ public class GameStage extends MyStage {
     @Override
     public void draw() {
         super.draw();
-        controlStage.draw();
+        //controlStage.draw();
     }
 
 
@@ -67,11 +117,9 @@ public class GameStage extends MyStage {
     @Override
     public void act(float delta) {
         super.act(delta);
-        controlStage.act(delta);
+        world.step(delta, 10, 10);
+        elapsedTime += delta;
 
     }
 
-    public ControlStage getControlStage() {
-        return controlStage;
-    }
 }
