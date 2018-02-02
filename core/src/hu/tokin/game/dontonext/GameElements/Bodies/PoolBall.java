0d0907a88cd4e72012f9e4f3ab2562fe03afdeff
mrclsu <marcell.schuh@gmail.com>
2018-02-02 10:01:26 +0100

    Szar

---
+++ core/src/hu/tokin/game/dontonext/GameElements/Bodies/PoolBall.java
@@ -0,0 +1,11 @@
+package hu.tokin.game.dontonext.GameElements.Bodies;
+
+import hu.tokin.game.dontonext.MyBaseClasses.Box2dWorld.WorldActorGroup;
+
+/**
+ * Created by M on 2/2/2018.
+ */
+
+public class PoolBall extends WorldActorGroup {
+    
+}
---
+++ core/src/hu/tokin/game/dontonext/GameElements/PoolTabel.java
@@ -0,0 +1,10 @@
+package hu.tokin.game.dontonext.GameElements;
+
+import hu.tokin.game.dontonext.MyBaseClasses.Scene2D.OneSpriteStaticActor;
+
+/**
+ * Created by M on 2/2/2018.
+ */
+
+public class PoolTabel extends OneSpriteStaticActor {
+}
